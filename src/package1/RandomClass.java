75404119376e877e51af9194f3242aff9e7c7f7f
Ryan McGee <ramtech51@comcast.net>
2018-07-24 19:20:06 -0400

    First commit of the training project

---
+++ src/package1/RandomClass.java
@@ -0,0 +1,21 @@
+package package1;
+
+/**
+ * 1. Create another package called package2 in the project.
+ * 2. Create a class inside package2, named TestingClass
+ * 3. Inside this class, make a method called testingMethod that calls method1
+ * here.
+ * 
+ * Tell us what went wrong, and what needs to change.
+ *
+ */
+public class RandomClass
+{
+private static int myInt = 0;
+
+private static void method1 ()
+{
+    System.out.println("RandomClass.method1() called! Yay!");
+}
+
+}
